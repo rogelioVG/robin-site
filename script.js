508464d9985d5960a3481602620fde812d0c866a
Rogelio <rogelio.vg@icloud.com>
2017-02-06 17:01:36 -0600

    ajax form

---
+++ script.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+    $('#someForm').on('submit', function(e) {
+        e.preventDefault();
+
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+        //get the comments
+        var comments = $('#comments').val();
+
+        //pretend we don't need validation
+
+        //send to formspree
+        $.ajax({
+            url:'https://formspree.io/raymondcamden@gmail.com',
+            method:'POST',
+            data:{
+                name:name,
+                _replyto:email,
+                 email:email,
+                comments:comments,
+                _subject:'My Form Submission',
+            },
+            dataType:"json",
+            success:function() {
+                console.log('success');
+                $('#formBlock').hide();
+                $('#thankyouBlock').show();
+            }
+
+        });
+
+    });
+
+});
